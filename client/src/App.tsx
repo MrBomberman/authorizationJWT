054a4043db2dd389dad7b85d68df36ba6d554ca0
Kirill <mrbomberman99@gmail.com>
2022-01-24 23:02:59 +0300

    created main page which will change in case of state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import LoginForm from "./components/LoginForm";
 import './App.css';
+import { useContext, useEffect, useState } from "react";
+import { Context } from "./index";
+import { observer } from "mobx-react-lite";
+import {IUser} from './models/response/IUser';
+import UserService from "./services/UserService";
 
 function App() {
+  const {store} = useContext(Context);
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    if(localStorage.getItem('token')) { // если в локалсторейдж по ключу есть токен, то вызываем функцию проверки аутентификации
+      store.checkAuth();
+    }
+  }, [])
+
+  async function getUsers(){
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  if(store.isLoading){
+    return <div>Loading...</div>
+  }
+
+  if(!store.isAuth) {
+    return (
+      <LoginForm/>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{store.isAuth == true ? `User was authorized ${store.user.email}` : 'You need to authorize'}</h1>
+      <h4>{store.user.isActivated == true ? `Your account was verified` : 'Verify your account!!!'}</h4>
+      <button onClick={() => store.logout()}>Exit</button>
+      <div>
+        <button onClick={getUsers}>Get all users</button>
+      </div>
+      {users.map(user => {
+        return <li key={user.email}>{user.email}</li>
+      })}
     </div>
   );
 }
 
-export default App;
+export default observer(App);
