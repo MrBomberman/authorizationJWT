fb518bec05174881b359f45b309b767e01820e89
Kirill <mrbomberman99@gmail.com>
2022-01-24 23:07:38 +0300

    added email service for server

--- server/service/emailService.js
+++ server/service/emailService.js
@@ -1,5 +1,34 @@
-class EmailService {
+const nodemailer = require('nodemailer');
 
+class EmailService {
+    constructor(){
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD
+            }
+
+        })
+    }
+
+    async sendActivationEmail(email , link){
+        await this.transporter.sendMail({
+            from : process.env.SMTP_USER,
+            to: email,
+            subject: 'Activation of account ' + process.env.API_URL,
+            text: '',
+            html: 
+                `
+                <div>
+                    <h1>To activate your account you need to go by this link</h1>
+                    <a href="${link}">${link}</a>
+                </div>
+                `
+        })
+    }
 }
 
 module.exports = new EmailService();
