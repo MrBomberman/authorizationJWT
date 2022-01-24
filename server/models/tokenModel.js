d30e1ce71aad851f2668da4ae6c25ebefb73f0cc
Kirill <mrbomberman99@gmail.com>
2022-01-24 23:10:06 +0300

    added user and token model to interact with mongo db

---
+++ server/models/tokenModel.js
@@ -0,0 +1,10 @@
+// to keep refresh token and id of user and also IP of adress ... other useful things
+// link of user and refresh token
+const {Schema, model} = require('mongoose');
+
+const TokenSchema = new Schema({
+    user: {type: Schema.Types.ObjectId, ref: 'UserSchema'},
+    refreshToken: {type: String, required: true},
+})
+
+module.exports = model('tokens', TokenSchema);
